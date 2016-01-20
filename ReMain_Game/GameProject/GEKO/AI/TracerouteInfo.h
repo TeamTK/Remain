f2b26a953ddb106d78685f957acd6d7908695361
GEKO <tooru000666@gmail.com>
2016-01-20 19:08:47 +0900

    Ai

--- ReMain_Game/GameProject/GEKO/AI/TracerouteInfo.h
+++ ReMain_Game/GameProject/GEKO/AI/TracerouteInfo.h
@@ -4,21 +4,52 @@
 #include "..\System\Math.h"
 #include "..\Mesh\StaticMesh.h"
 
+//ポリゴンの隣接情報
 struct PolyLinkInfo
 {
-	int linkPolyIndex[3];
-	float linkPolyDistance[3];
-	Vector3D centerPosition;
+	int linkPolyIndex[3];		 //隣接しているポリゴン番号
+	float linkPolyDistance[3]; //隣接しているポリゴンの中心座標までの距離
+	Vector3D centerPosition;	 //ポリゴンの中心座標
 };
 
+//経路探索処理用の1ポリゴンの情報
+struct PathPlanningUnit
+{
+	int polyIndex;						 //ポリゴン番号
+	int prevPolyIndex;					 //経路探索でこのポリゴンに到達するまでに通過したポリゴン間の距離の合計
+	int NextPolyIndex;					 //経路探索で確定した経路上の一つ前のポリゴン( 当ポリゴンが経路上に無い場
+	float totalDistance;				 //経路探索で確定した経路上の一つ先のポリゴン( 当ポリゴンが経路上に無い場
+	PathPlanningUnit *pActiveNextUnit; //経路探索処理対象になっている次のポリゴンのメモリアドレスを格納する変数
+};
+
+//経路探索処理で使用する情報を保存する為の構造体
+struct PathPlanning
+{
+	PathPlanningUnit *pUnitArray;		  //経路探索処理で使用する全ポリゴンの情報配列が格納されたメモリ領域の先頭メモリアドレスを格納する変数
+	PathPlanningUnit *pActiveFirstUnit; //経路探索処理対象になっているポリゴン群の最初のポリゴン情報へのメモリアドレスを格納する変数
+	PathPlanningUnit *pStartUnit;		  //経路のスタート地点にあるポリゴン情報へのメモリアドレスを格納する変数
+	PathPlanningUnit *pGoalUnit;		  //経路のゴール地点にあるポリゴン情報へのメモリアドレスを格納する変数
+};
+
+struct PathMoveInfo
+{
+	int nowPolyIndex;							  //現在乗っているポリゴンの番号
+	Vector3D nowPosition;						  //現在位置
+	Vector3D MoveDirection;					  //移動方向
+	PathPlanningUnit *pNowPathPlanningUnit;	  //現在乗っているポリゴンの経路探索情報が格納されているメモリアドレスを格納する変数
+	PathPlanningUnit *pTargetPathPlanningUnit; //次の中間地点となる経路上のポリゴンの経路探索情報が格納されているメモリアドレスを格納する変数
+};
+
+//経路情報
 struct TracerouteInfo
 {
 	StaticMesh *pStaticMesh;
 	PolyLinkInfo *pPoryLinkInfo;
+	PathPlanning pathPlanning;
 
 	TracerouteInfo() :
 		pStaticMesh(nullptr),
-		pPoryLinkInfo(nullptr){};
+		pPoryLinkInfo(nullptr) {};
 };
 
 #endif
--- ReMain_Game/GameProject/GEKO/AI/TracerouteManager.h
+++ ReMain_Game/GameProject/GEKO/AI/TracerouteManager.h
@@ -2,6 +2,7 @@
 #define _TRACEROUTE_MANAGER_H_
 
 class StaticMesh;
+class Vector3D;
 
 class TracerouteManager
 {
@@ -11,11 +12,14 @@ public:
 	static void Clear(const char* name);
 	static void AlClear();
 	static void DebugRender(const char* name);
+	static void DebugMigrationPath(const char* name);
 	static void ProcessBuilding(const char* name);
+	static void SetupPathPlanning(const char* name, const Vector3D &startPos, const Vector3D &goalPos);
 
 private:
 	TracerouteManager();
 	static TracerouteManager* GetInstance();
+	int CheckOnPolyIndex(StaticMesh *pStaticMesh, const Vector3D &pos);
 
 private:
 	class TraceroutePimpl;
